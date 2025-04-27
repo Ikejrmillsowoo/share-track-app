013abcce2f50f2621403fc5f029f34a4b7c7ddea
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-04-27 06:35:04 -0400

    location and item repos to get items in a location

--- src/main/java/com/passion_share_track/share_track/repositories/ItemRepository.java
+++ src/main/java/com/passion_share_track/share_track/repositories/ItemRepository.java
@@ -1,14 +1,15 @@
 package com.passion_share_track.share_track.repositories;
 
 import com.passion_share_track.share_track.models.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
+//jpa
 @Repository
-public interface ItemRepository extends CrudRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long> {
 
     //Find all items by location Id
     List<Item> findByLocationId(Long locationid);
--- src/main/java/com/passion_share_track/share_track/repositories/LocationListRepository.java
+++ src/main/java/com/passion_share_track/share_track/repositories/LocationListRepository.java
@@ -1,7 +1,8 @@
 package com.passion_share_track.share_track.repositories;
 
 import com.passion_share_track.share_track.models.LocationList;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface LocationListRepository extends CrudRepository<LocationList, Long> {
+public interface LocationListRepository extends JpaRepository<LocationList, Long> {
 }
