ac69542c7fbd83554a2a4ea526beccc6772d57d2
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-04-27 20:52:37 -0400

    still working on user model

--- src/main/java/com/passion_share_track/share_track/models/User.java
+++ src/main/java/com/passion_share_track/share_track/models/User.java
@@ -1,18 +1,70 @@
 package com.passion_share_track.share_track.models;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+@Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "USER_ID")
     private Long id;
-
+    @Column(name = "FIRST_NAME")
     private String firstName;
+    @Column(name = "LAST_NAME")
     private String lastName;
+    @Column(name = "USER_ROLE")
     private String userRole;
 
+    @Column(name = "LOCATION_ID")
     private Long locationId;
 
+    public User(Long id, String firstName, String lastName, String userRole, Long locationId) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userRole = userRole;
+        this.locationId = locationId;
+    }
+
+    public User(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    public Long getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(Long locationId) {
+        this.locationId = locationId;
+    }
 }
