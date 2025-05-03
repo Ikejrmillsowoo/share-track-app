32af34083611a3b2045844b7827046a0d487a38a
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-05-02 22:52:54 -0400

    updated

--- src/main/java/com/passion_share_track/share_track/MvcConfig.java
+++ src/main/java/com/passion_share_track/share_track/MvcConfig.java
@@ -1,4 +1,14 @@
-package com.passion_share_track.share_track;
-
-public class MvcConfig {
-}
+//package com.passion_share_track.share_track;
+//
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//public class MvcConfig implements WebMvcConfigurer {
+//
+//    public void addViewControllers(ViewControllerRegistry registry){
+//        registry.addViewController("/index").setViewName("index");
+//        registry.addViewController("/").setViewName("index");
+//        registry.addViewController("/items").setViewName("items");
+//        registry.addViewController("/signup").setViewName("signup");
+//    }
+//}
