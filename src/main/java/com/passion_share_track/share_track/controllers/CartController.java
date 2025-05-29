08cc06c9414d2081df413e8487c3f17434f7019e
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-05-29 18:49:54 -0400

    updating get items

--- src/main/java/com/passion_share_track/share_track/controllers/CartController.java
+++ src/main/java/com/passion_share_track/share_track/controllers/CartController.java
@@ -1,10 +1,13 @@
 package com.passion_share_track.share_track.controllers;
 
 import com.passion_share_track.share_track.models.Cart;
+import com.passion_share_track.share_track.models.CartItem;
 import com.passion_share_track.share_track.services.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cart")
@@ -19,12 +22,28 @@ public class CartController {
         return ResponseEntity.ok(newCart);
     }
 
+    // Add item to cart
+    @PostMapping("/{cartId}/add-item")
+    public ResponseEntity<Cart> addItemToCart(
+            @PathVariable Long cartId,
+            @RequestParam Long itemId,
+            @RequestParam int quantity) {
+        Cart updatedCart = cartService.addItemToCart(cartId, itemId, quantity);
+        return ResponseEntity.ok(updatedCart);
+    }
+
     // get a user's cart
-    @PostMapping("/user/{userId}")
+    @GetMapping("/user/{userId}")
     public ResponseEntity<Cart> getCartByUser(@RequestParam Long userId){
         Cart cart = cartService.getCartByUserId(userId);
         return ResponseEntity.ok(cart);
     }
 
 
+    // Get all items in a cart
+    @GetMapping("/{cartId}/items")
+    public ResponseEntity<List<CartItem>> getCartItems(@PathVariable Long cartId) {
+        List<CartItem> items = cartService.getItemsInCart(cartId);
+        return ResponseEntity.ok(items);
+    }
 }
